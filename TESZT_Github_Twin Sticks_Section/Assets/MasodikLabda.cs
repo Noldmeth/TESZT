245ebeaf66ce6ca6156c87d5c6410566ab1dbd91
Noldmeth <n.arni93@gmail.com>
2019-04-01 20:44:02 +0200

    Megszűnt a Temp mappa figyelése, a Libary még mindig bennet van, nem tudom ez gond-e

--- TESZT_Github_Twin Sticks_Section/Assets/MasodikLabda.cs
+++ TESZT_Github_Twin Sticks_Section/Assets/MasodikLabda.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyScript : MonoBehaviour {
+public class MasodikLabda : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,8 +11,10 @@ public class MyScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown()) {
-			print ("egér megnyomva!");
-		}
+
+
+		//Ez egy szemléltető módosítás!
+	
+	
 	}
 }
